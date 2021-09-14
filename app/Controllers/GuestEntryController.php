a2908dea6c540e7d378bdc2ad9395df0884a9feb
Isaac Colls <colls_isaac@hotmail.com>
2021-09-14 19:39:57 -0300

    feat: validator when create-guest

--- app/Controllers/GuestEntryController.php
+++ app/Controllers/GuestEntryController.php
@@ -5,18 +5,24 @@ namespace App\Controllers;
 use App\Models\GuestEntry;
 use App\Requests\CustomRequestHandler;
 use App\Response\CustomResponse;
+use App\Validation\Validator;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\RequestInterface as Request;
+use Respect\Validation\Exceptions\NestedValidationException;
+use Respect\Validation\Validator as v;
+
 
 class GuestEntryController
 {
   protected $customResponse;
   protected $guestEntry;
+  protected $validator;
 
   public function __construct()
   {
     $this->customResponse = new CustomResponse();
     $this->guestEntry = new GuestEntry();
+    $this->validator = new Validator();
   }
 
   public function health(Request $request, Response $response)
@@ -27,6 +33,15 @@ class GuestEntryController
 
   public function createGuest(Request $request, Response $response)
   {
+    $this->validator->validate($request, [
+      "full_name" => v::notEmpty(),
+      "email" => v::notEmpty()->email(),
+      "comment" => v::notEmpty()
+    ]);
+    if ($this->validator->failed()) {
+      $responseMessage = $this->validator->errors;
+      return $this->customResponse->is400Response($response, $responseMessage);
+    }
     $this->guestEntry->create([
       'full_name' => CustomRequestHandler::getParam($request, 'full_name'),
       'email' => CustomRequestHandler::getParam($request, 'email'),
